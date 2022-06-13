d1c71de1597a12f2301136347a6f8e4f84214fbd
Natalia <dnataliacr@gmail.com>
2022-06-12 22:23:56 -0300

    aggregate context provider and router in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import ReactDOM  from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import App from "./App.js";
+import { ResultContextProvider } from "./contexts/ResultContextProvider.js";
+import "./index.css";
 
-import App from './App.js'
-import './index.css'
+ReactDOM.render(
+  <ResultContextProvider>
+  <Router>
+    {" "}
+    <App />
+  </Router>
+  </ResultContextProvider>,
 
-ReactDOM.render(<App />, document.getElementById('root'))
+  document.getElementById("root")
+);
